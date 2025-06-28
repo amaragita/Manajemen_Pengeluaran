717d467662ab92d3b641a7f4e9a13589535855eb
amaragita tiarani <amaragita@student.undiksha.ac.id>
2025-06-28 14:05:07 +0800

    eleven commit

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,5 +1,4 @@
 class Expense {
-  final int? id;
   final String description;
   final double amount;
   final DateTime date;
@@ -7,33 +6,10 @@ class Expense {
   final String? imagePath;
 
   Expense({
-    this.id,
     required this.description,
     required this.amount,
     required this.date,
     required this.category,
     this.imagePath,
   });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'description': description,
-      'amount': amount,
-      'date': date.toIso8601String(),
-      'category': category,
-      'imagePath': imagePath,
-    };
-  }
-
-  factory Expense.fromMap(Map<String, dynamic> map) {
-    return Expense(
-      id: map['id'],
-      description: map['description'],
-      amount: map['amount'],
-      date: DateTime.parse(map['date']),
-      category: map['category'],
-      imagePath: map['imagePath'],
-    );
-  }
 } 
